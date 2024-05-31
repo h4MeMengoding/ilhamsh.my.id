abe9f6f710ba742d8472ee629ceed15af161d972
h4MeMengoding <ilhamshofaa@gmail.com>
2024-05-31 08:49:16 +0700

    feat: fix error Hydration failed

--- src/common/components/layouts/partials/Sidebar.tsx
+++ src/common/components/layouts/partials/Sidebar.tsx
@@ -2,17 +2,19 @@ import { useEffect, useState } from 'react';
 
 import useIsMobile from '@/common/hooks/useIsMobile';
 
-import Breakline from '../../elements/Breakline';
 import SearchBox from '../../elements/SearchBox';
 import ThemeSwitcher from '../../elements/ThemeSwitcher';
 import Navigation from '../../sidebar/Navigation';
 import Profile from '../../sidebar/Profile';
 
 const Sidebar = () => {
-  const isMobile = useIsMobile();
+  const isMobile = useIsMobile(); // Call at the top level
   const [isScrolled, setIsScrolled] = useState(false);
+  const [hasMounted, setHasMounted] = useState(false); // Track if component has mounted
 
   useEffect(() => {
+    setHasMounted(true); // Set mounted state to true once mounted
+
     const handleScroll = () => {
       const scrollTop =
         window.pageYOffset || document.documentElement.scrollTop;
@@ -26,10 +28,21 @@ const Sidebar = () => {
     };
   }, []);
 
+  // Conditional rendering based on mount status to safely manage rendering during SSR
+  if (!hasMounted) {
+    return (
+      <div
+        id='sidebar'
+        className='sticky top-0 z-10 flex flex-col space-y-6 transition-all duration-300 lg:py-6'
+      >
+        {/* Placeholder or loader can go here if needed */}
+      </div>
+    );
+  }
+
   return (
     <div
       id='sidebar'
-      // className='flex flex-col space-y-6 transition-all duration-300 lg:py-8'
       className='sticky top-0 z-10 flex flex-col space-y-6 transition-all duration-300 lg:py-6'
     >
       <Profile isScrolled={isScrolled} />
@@ -39,7 +52,6 @@ const Sidebar = () => {
             <SearchBox />
           </div>
           <Navigation />
-          <Breakline className='mx-1' />
           <div className='space-y-2.5 px-1'>
             <div className='px-3'>
               <span className='text-sm text-neutral-600'>Theme</span>
